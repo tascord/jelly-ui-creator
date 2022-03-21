7ca190216368ed0fde11b8c286880c12894b80d8
Flora Hill <theactivesquare@gmail.com>
2022-03-21 12:19:21 +1100

    UI

--- designer.js
+++ designer.js
@@ -1,33 +1,196 @@
 // C, X
 
 const Root = new Renderable();
+let selected = Root;
 
-const base = new Pane(
-    'base',
-    'navy.900.75',
-    0,
-    0,
-    scaled({ rem_width: 90, rem_height: 50, min_width: 20, min_height: 20, }),
-    'center-center',
-);
+document.querySelector('.button').onclick = () => {
+    document.querySelector('.button').classList.toggle('closed');
+    document.querySelector('nav').classList.toggle('closed');
+}
 
-const button = new Button(
-    'submit',
-    'Hello world!',
-    'navy.300.100',
-    0, 0,
-    scaled({ rem_width: 20, rem_height: 10, min_width: 20, min_height: 10, }),
-    'center-center',
-)
+const wu = document.getElementById('width-units');
+const hu = document.getElementById('height-units');
+const wm = document.getElementById('width-min');
+const hm = document.getElementById('height-min');
+const p = document.getElementById('parent');
+const a = document.getElementById('alignment');
+const c = document.getElementById('colour');
+const t = document.getElementById('text');
+const x = document.getElementById('x');
+const y = document.getElementById('y');
 
-Context.on('submit.click', () => {
-    Context.emit('submit.text', 'Goodbye, world!');
-})
+const s = document.getElementById('selected');
+const i = document.getElementById('items');
 
-Context.on('submit.hover', () => {
-    Context.emit('submit.colour', 'ochre.300.100');
-})
+const p_ = document.getElementById('palette');
 
-Root.add_child(base);
-base.add_child(button);
+function select(node) {
+
+    selected = node;
+    update();
+
+    if (selected === Root) {
+        s.innerText = 'Root';
+        wu.disabled = true;
+        hu.disabled = true;
+        wm.disabled = true;
+        hm.disabled = true;
+        x.disabled = true;
+        y.disabled = true;
+        p.disabled = true;
+        a.disabled = true;
+        c.disabled = true;
+        t.disabled = true;
+        return;
+    } else {
+        wu.disabled = false;
+        hu.disabled = false;
+        wm.disabled = false;
+        hm.disabled = false;
+        x.disabled = false;
+        y.disabled = false;
+        p.disabled = false;
+        a.disabled = false;
+        c.disabled = false;
+        t.disabled = node.text === undefined;
+    }
+
+    s.innerText = node.name?.toString() ?? 'Unknown node';
+    wu.value = node.scale.unit_width?.toString() ?? '0';
+    hu.value = node.scale.unit_height?.toString() ?? '0';
+    wm.value = node.scale.min_width?.toString() ?? '0';
+    hm.value = node.scale.min_height?.toString() ?? '0';
+    c.value = node.colour?.toString() ?? '';
+    a.value = node.alignment?.toString() ?? '';
+    x.value = node.x?.toString() ?? '';
+    y.value = node.y?.toString() ?? '';
+
+    if(t.text) t.innerText = node.text;
+
+    const self = [...p.children].find(p => p.value === node.name);
+    if (self && self.delete) self.delete();
+
+    const parent = [...p.children].find(p => p.value === node.parent)
+    if (parent) parent.selected = true;
+
+    p.value = node.parent === Root ? 'root' : node.parent.name;
+
+}
+
+function update() {
+
+    while (i.firstChild) {
+        i.removeChild(i.firstChild);
+    }
+
+    while (p.firstChild) {
+        p.removeChild(p.firstChild);
+    }
+
+    const item = document.createElement('li');
+    item.innerText = 'Root';
+    item.onclick = () => select(Root);
+    i.appendChild(item);
+
+    const option = document.createElement('option');
+    option.value = 'root';
+    option.innerText = 'Root';
+    p.appendChild(option);
+
+    propagate(Root, (child => {
+        if (!child.name) return;
+
+        const del = document.createElement('button');
+        del.innerText = 'Delete';
+        del.onclick = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            child.parent.children = child.parent.children.filter(child => child.name !== child.name);
+            select(child.parent);
+        }
+
+        const item = document.createElement('li');
+        item.innerText = child.name;
+        item.onclick = () => select(child);
+        item.appendChild(del);
+        i.appendChild(item);
+
+        const option = document.createElement('option');
+        option.value = child.name;
+        option.innerText = child.name;
+        p.appendChild(option);
+
+    }));
+
+}
+
+select(Root);
+
+for (const part of Object.keys(Parts)) {
+    const option = document.createElement('option');
+    option.innerText = 'New ' + part;
+    option.value = part;
+    p_.appendChild(option);
+}
+
+document.getElementById('create').onclick = () => {
+
+    const part_name = p_.value;
+    const part = Parts[part_name];
+
+    const name = prompt(`New ${part_name} name. (MUST BE UNIQUE)`);
+    if (!name) return alert(`No name provided. Aborting.`);
+
+    const node = new part({
+        name,
+        colour: 'navy.200.100',
+        alignment: a.value
+    });
+
+    selected.add_child(node);
+    select(node);
+
+}
+
+// Handlers
+
+wu.onchange = (e) => selected.scale.unit_width = parseFloat(e.target.value);
+hu.onchange = (e) => selected.scale.unit_height = parseFloat(e.target.value);
+wm.onchange = (e) => selected.scale.min_width = parseFloat(e.target.value);
+hm.onchange = (e) => selected.scale.min_height = parseFloat(e.target.value);
+
+a.onchange = (e) => selected.alignment = e.target.value;
+c.onchange = (e) => selected.colour = e.target.value;
+t.onchange = (e) => selected.text = e.target.value;
+
+x.onchange = (e) => selected.x = parseFloat(e.target.value);
+y.onchange = (e) => selected.y = parseFloat(e.target.value);
+
+// Parent
+p.onchange = (e) => {
+
+    if (e.target.value === 'root') {
+        const old_parent = selected.parent;
+        selected.parent = Root;
+        old_parent.children = old_parent.children.filter(child => child !== selected);
+    }
+
+    propagate(Root, (child => {
+
+        if (!child.name) return;
+        if (child.name !== e.target.value) return;
+
+        // Store old parent
+        const old_parent = selected.parent;
+
+        // Add to new parent
+        selected.parent = child;
+
+        // Remove from old parent
+        old_parent.children = old_parent.children.filter(child => child.name !== selected.name);
+
+    }));
+
+}
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const C = document.querySelector('canvas'), X = C.getContext('2d');
 let MX = 0, MY = 0;
-let REM = 0;
+let UNITS = 0;
 
 const resize = () => {
     const { width, height } = C.getBoundingClientRect();
@@ -8,7 +8,7 @@ const resize = () => {
     C.width = width;
     C.height = height;
 
-    REM = (width + height) / 2 / 100;
+    UNITS = (width + height) / 2 / 100;
 
 }
 
@@ -29,6 +29,8 @@ document.body.onmousemove = (e) => {
         const { x, y } = alignment(child.alignment, child.x, child.y, child.scale, child.calc_parent());
         if (MX >= x && MX <= x + child.scale.get_width() && MY >= y && MY <= y + child.scale.get_height()) {
             Context.emit(child.name + '.hover', { x, y });
+        } else {
+            Context.emit(child.name + '.unhover', { x, y });
         }
     }))
 
@@ -46,9 +48,11 @@ document.body.onclick = (e) => {
 
 let loaded = false;
 const image = document.querySelector('img');
-image.onload = () => {
-    loaded = true;
-    image.style.display = 'none';
+if (image) {
+    image.onload = () => {
+        loaded = true;
+        image.style.display = 'none';
+    }
 }
 
 resize();
@@ -61,141 +65,17 @@ setInterval(() => {
 
 }, 150);
 
-const Colours = {
-    navy: {
-        '50': '#f2f7f8',
-        '100': '#d9eff9',
-        '200': '#aee0f1',
-        '300': '#7ac3df',
-        '400': '#43a0c6',
-        '500': '#3281ae',
-        '600': '#2a6795',
-        '700': '#244e76',
-        '800': '#1a3555',
-        '900': '#112139',
-    },
-    blue: {
-        '50': '#f7f9fa',
-        '100': '#e8effa',
-        '200': '#cfd9f5',
-        '300': '#a8b6e7',
-        '400': '#838ed6',
-        '500': '#6a6bc7',
-        '600': '#5750b1',
-        '700': '#433c8d',
-        '800': '#2e2964',
-        '900': '#1b1a3e',
-    },
-    indigo: {
-        '50': '#f9fafa',
-        '100': '#f0eff8',
-        '200': '#e0d6f2',
-        '300': '#c2b0e1',
-        '400': '#a986cb',
-        '500': '#8f63b8',
-        '600': '#76479e',
-        '700': '#59357b',
-        '800': '#3e2554',
-        '900': '#241832',
-    },
-    mauve: {
-        '50': '#fbfbfa',
-        '100': '#f8eff1',
-        '200': '#f1d2e4',
-        '300': '#dfa8c6',
-        '400': '#d57ba4',
-        '500': '#c15886',
-        '600': '#a63d67',
-        '700': '#802e4c',
-        '800': '#5a2033',
-        '900': '#36151d',
-    },
-    beaver: {
-        '50': '#fbfbf9',
-        '100': '#f8efe9',
-        '200': '#f1d5d1',
-        '300': '#e0aca8',
-        '400': '#d2807c',
-        '500': '#bc5d59',
-        '600': '#a0423f',
-        '700': '#7b3230',
-        '800': '#552321',
-        '900': '#351614',
-    },
-    ochre: {
-        '50': '#fbfaf6',
-        '100': '#f9efd1',
-        '200': '#f1d9a4',
-        '300': '#deb370',
-        '400': '#c78844',
-        '500': '#ac6727',
-        '600': '#8f4d1a',
-        '700': '#6e3a16',
-        '800': '#4c2811',
-        '900': '#31190c',
-    },
-    lemon: {
-        '50': '#faf9f4',
-        '100': '#f7efc9',
-        '200': '#eddd95',
-        '300': '#d3b961',
-        '400': '#b09038',
-        '500': '#90711e',
-        '600': '#755714',
-        '700': '#5a4211',
-        '800': '#3d2d0f',
-        '900': '#291c0b',
-    },
-    olive: {
-        '50': '#f9f9f4',
-        '100': '#f4efd3',
-        '200': '#e7dea5',
-        '300': '#c8bc70',
-        '400': '#9c9444',
-        '500': '#7b7526',
-        '600': '#635c19',
-        '700': '#4d4516',
-        '800': '#353012',
-        '900': '#231e0d',
-    },
-    viridian: {
-        '50': '#f4f7f5',
-        '100': '#e3efee',
-        '200': '#c1e2d9',
-        '300': '#8dc4b2',
-        '400': '#4ea287',
-        '500': '#388460',
-        '600': '#2f6c48',
-        '700': '#285239',
-        '800': '#1d382b',
-        '900': '#13231f',
-    },
-    teal: {
-        '50': '#f2f7f6',
-        '100': '#daeff5',
-        '200': '#afe2ea',
-        '300': '#79c5cf',
-        '400': '#3ea3ad',
-        '500': '#2d868d',
-        '600': '#276d72',
-        '700': '#225359',
-        '800': '#193941',
-        '900': '#10232d',
-    },
-
-}
-
-function scaled({ rem_width, rem_height, min_width, min_height }) {
+function scaled({ unit_width, unit_height, min_width, min_height }) {
     return {
 
-        rem_width,
-        rem_height,
+        unit_width,
+        unit_height,
 
         min_width,
         min_height,
 
-        get_width: () => Math.max(rem_width * REM, min_width),
-        get_height: () => Math.max(rem_height * REM, min_height),
+        get_width: function () { return Math.max(this.unit_width * UNITS, this.min_width) },
+        get_height: function () { return Math.max(this.unit_height * UNITS, this.min_height) },
 
     }
 }
@@ -239,8 +119,8 @@ function export_data() {
                 x: c.x,
                 y: c.y,
 
-                width: [c.scale.rem_width, c.scale.min_width],
-                height: [c.scale.rem_height, c.scale.min_height],
+                width: [c.scale.unit_width, c.scale.min_width],
+                height: [c.scale.unit_height, c.scale.min_height],
 
                 colour: c.colour,
                 alignment: c.alignment,
@@ -255,12 +135,8 @@ function export_data() {
 
     element.style.display = 'none';
     document.body.appendChild(element);
-
     element.click();
-
     document.body.removeChild(element);
-    c.href = window.URL.createObjectURL(t);
-    c.click();
 }
 
 function import_data(data) {
@@ -268,24 +144,26 @@ function import_data(data) {
 
     const parse = (children, parent) => children.map(c => {
 
+        console.log(`PARSE`, children, parent);
+
         let type;
 
         if (c.type === 'pane') type = Pane;
         else throw new Error('Unknown type: ' + c.type);
 
-        const [rem_width, min_width] = c.width;
-        const [rem_height, min_height] = c.height;
+        const [unit_width, min_width] = c.width;
+        const [unit_height, min_height] = c.height;
 
-        const scale = scaled({ rem_width, rem_height, min_width, min_height });
+        const scale = scaled({ unit_width, unit_height, min_width, min_height });
 
-        const element = new type(
-            c.name,
-            c.colour,
-            c.x, c.y,
-            scale,
-            c.alignment,
-            parent
-        );
+        const element = new type({
+            name: c.name,
+            colour: c.colour,
+            x: c.x, y: c.y,
+            scale: scale,
+            alignment: c.alignment,
+            parent: parent
+        });
 
         element.children = parse(c.children ?? [], element);
         return element;
@@ -293,7 +171,12 @@ function import_data(data) {
     })
 
     Root.children = parse(root.children, Root);
+    select(Root);
 
+}
+
+document.getElementById('import').onclick = () => {
+    import_data(prompt('Paste JSON data here'));
 }
 
 const Context = {
@@ -310,6 +193,8 @@ const Context = {
         (this.handlers[type] ?? []).forEach(h => h(data));
     }
 }
+
+/** --------------------------------- **/
 
 class Renderable {
     constructor(parent) {
@@ -333,17 +218,17 @@ class Renderable {
 }
 
 class Pane extends Renderable {
-    constructor(name, colour, x, y, scale, alignment, parent) {
-        super('pane', name, parent);
+    constructor({ name, colour, x, y, scale, alignment, parent }) {
+        super(parent);
         this.type = 'pane';
 
-        this.x = x;
-        this.y = y;
+        this.x = x ?? 0;
+        this.y = y ?? 0;
 
         this.name = name;
-        this.colour = colour;
-        this.scale = scale;
-        this.alignment = alignment;
+        this.colour = colour ?? 'navy.300.100';
+        this.scale = scale ?? scaled({ unit_width: 1, unit_height: 1, min_width: 1, min_height: 1 });
+        this.alignment = alignment ?? 'center-center';
 
         Context.on(this.name + '.colour', (colour) => this.colour = colour);
         Context.on(this.name + '.scale', (scale) => this.scale = scale);
@@ -384,11 +269,11 @@ class Pane extends Renderable {
 }
 
 class Button extends Pane {
-    constructor(name, text, colour, x, y, scale, alignment, parent) {
+    constructor({ name, text, colour, x, y, scale, alignment, parent }) {
 
-        super(name, colour, x, y, scale, alignment, parent);
+        super({ name, colour, x, y, scale, alignment, parent });
 
-        this.text = text;
+        this.text = text ?? 'Button';
         this.type = 'button';
 
         Context.on(this.name + '.text', (text) => this.text = text);
@@ -416,4 +301,9 @@ class Button extends Pane {
 
     }
 
+}
+
+const Parts = {
+    "Pane": Pane,
+    "Button": Button
 }
